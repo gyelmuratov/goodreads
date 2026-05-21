b41e12e6af70771b823c9faef0f48ada1222911b
Gulam Yelmuratov <gulamyelmuratov6@gmail.com>
2026-05-21 12:41:51 +0500

    update tasks

--- users/signals.py
+++ users/signals.py
@@ -1,18 +1,13 @@
-from django.contrib.auth.models import User
-from django.core.mail import send_mail
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-
 from users.models import CustomUser
-
 
 @receiver(post_save, sender=CustomUser)
 def send_welcome_email(sender, instance, created, **kwargs):
     if created:
-        send_mail(
+        from users.tasks import send_email
+        send_email.delay(
             "Welcome to Goodreads",
             f"Hi {instance.username}! Welcome to Goodreads",
-            "gulamyelmuratov6@gmail.com",
             [instance.email],
-
         )
